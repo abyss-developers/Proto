a80c19b0e6a95a6f9f300141aefbbe559922779b
Jason Zhou <37679610+jatgm@users.noreply.github.com>
2020-01-25 23:58:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from tkinter import *
+from PIL import ImageTk, Image
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
+
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
@@ -32,73 +35,142 @@ col.pop(0)
 
 # pprint(col2)
 
+"""End of thing"""
+
+root = Tk()
+root.title("PROTO: Pre-Alpha Release")
+root.geometry("1300x700")
+root.resizable(0, 0)
+my_img1 = ImageTk.PhotoImage(Image.open("etalabx03L.jpg"))
+root.configure(bg="#f2f2f2")
+
+background = Label(image=my_img1)
+background.pack(side=LEFT)
+
+frame = LabelFrame(root, text="PROTO", padx=5, pady=5, font=("Courier", 45), bg="#e6e6e6")
+frame.pack(padx=10, pady=10, side=RIGHT)
+
+usernameEntry = Entry(frame, width=40, bg="#e6e6e6", fg="Black", borderwidth=4)
+usernameEntry.grid(row=1, column=1, columnspan=2)
+usernameEntry.insert(0, "Enter Your Username:")  # Default text
+passwordEntry = Entry(frame, width=40, bg="#e6e6e6", fg="Black", borderwidth=4)
+passwordEntry.grid(row=2, column=1, columnspan=2)
+passwordEntry.insert(0, "Enter Your Password:")  # Default text
 
 
-def mainmenu(logUser, logPass):
-    global query
-    index = col.index("{}:{}".format(logUser, logPass))
-    index += 1
-    print("Choices: \"NewEntree\",\"MyFeed\" ")
-    e = input("Hello and welcome back to Proto! What would you like to do? ")
-    if e.lower().strip() == "newentree":
-        name = input("What is your name? ")
-        time = input("Please input the date. ")
-        print("New journal entree made. Please right your entree.")
-        entree = input()
-        print("Saving...")
-        finishedentree = ["{}\n{}\n{}".format(name, time, entree)]
-        sheet2.insert_row(finishedentree, 2)
-        print("TIP: To see your entree in MyFeed, restart the client.")
-        # makeaccount = ["{}:{}".format(username, password)]
-        # sheet.insert_row(makeaccount, 3)
-        mainmenu(logUser, logPass)
-    elif e.lower().strip() == "myfeed":
-        for c in col2:
-            print(c)
-        mainmenu(logUser, logPass)
+def writeEntree(nameText, dateText, bodyText):
+    writeName = nameText.get()
+    writeDate = dateText.get()
+    writeBody = bodyText.get()
+    finishedentree = ["{}\n{}\n{}".format(writeName, writeDate, writeBody)]
+    sheet2.insert_row(finishedentree, 2)
+    print("Done")
+
+def newentree():
+    entrefreme = Toplevel()
+    journalframe = LabelFrame(entrefreme, text="New Entree", padx=2, pady=2, font=("Helvetica", 45))
+    journalframe.pack(padx=10, pady=10, side=LEFT)
+    nameLabel = Label(journalframe, text="Write your name:")
+    dateLabel = Label(journalframe, text="Write the date:")
+    bodyLabel = Label(journalframe, text="Write your entree:")
+    nameText = Entry(journalframe, width=50, borderwidth=4)
+    dateText = Entry(journalframe, width=50, borderwidth=4)
+    bodyText = Entry(journalframe, width=80, borderwidth=4)
+    submit = Button(journalframe, text="Submit", fg="green", font=("Arial", 20), borderwidth=4, command=lambda: writeEntree(nameText, dateText, bodyText))
+
+    nameLabel.grid(row=1, column=1, columnspan=2)
+    nameText.grid(row=2, column=1, columnspan=2)
+    dateLabel.grid(row=3, column=1, columnspan=2)
+    dateText.grid(row=4, column=1, columnspan=2)
+    bodyLabel.grid(row=5, column=1, columnspan=2)
+    bodyText.grid(row=6, column=1, columnspan=2)
+    submit.grid(row=7,column=1,columnspan=2)
+
+    entrefreme.title("New PROTO Entree")
 
 
-def isAccount(logUser, logPass, num_lines, islist, search):
+def myClick():  # This gets ran whenever button is pressed
+    checkUser = usernameEntry.get()
+    checkPass = passwordEntry.get()
     for c in col:
-        if c == "{}:{}".format(logUser, logPass):
-            print("You have logged in: Method 2")
-        islist = num_lines - search
-        if col[islist] == "{}:{}".format(logUser, logPass):
-            print("You have logged in!")
-            mainmenu(logUser, logPass)
-        else:
-            search += 1
+        if c == "{}:{}".format(checkUser, checkPass):
+            print("User has logged in")
+            efe = Label(frame, text="You have logged in!", bg="#e6e6e6", fg="green")
+            efe.grid(row=4, column=1, columnspan=2)
+            top = Toplevel()
+            lbl = Label(top, text="Attribution: Jason Zhou")
+
+            clientframe = LabelFrame(top, text="Welcome to Proto.", padx=2, pady=2, font=("Helvetica", 45))
+            clientframe.pack(padx=10, pady=10)
+            test = Button(clientframe, text="New PROTO", width=33, height=20, borderwidth=6, font=("Helvetica", 18),
+                          command=newentree)
+            test.grid(row=1, column=1)
+            test = Button(clientframe, text="My Feed", width=33, height=20, borderwidth=6, font=("Helvetica", 18))
+            test.grid(row=1, column=2)
+
+            lbl.pack()
+            top.geometry("1000x700")
+            top.title("PROTO CLIENT")
+        elif c != "{}:{}".format(checkUser, checkPass):
+            rwwe = Label(frame, text="Incorrect username or password!", bg="#e6e6e6", fg="red")
+            rwwe.grid(row=4, column=1, columnspan=2)
 
 
-def login():
-    islist = 0
-    num_lines = len(col) - 1
-    print("Please log in.")
-    logUser = input("Username: ")
-    logPass = input("Password: ")
-    search = 0
-    isAccount(logUser, logPass, num_lines, islist, search)
+# Client Code
+
+
+my_label = Label(image=my_img1)
+my_label.pack()
+
+yesyes = 0
+
+
+def goback():
+    NoAccount = Button(frame, text="Don't have an account? \n Sign up now for free.", font=("Ariel", 10), bg="#e6e6e6",
+                       command=sign_in)  # command is what it does (Refer to function)
+    NoAccount.grid(row=3, column=1)
+    myButtone = Button(frame, text="Log In", command=myClick, width=12, height=2, font=("Ariel", 10),
+                       bg="#e6e6e6")
+    myButtone.grid(row=3, column=2)
+    return
 
 
 def register():
     global col
-    print("Sign up now for free!")
-    username = input("Please input a username. ")
-    password = input("Please input a password. ")
-    confirm = input("Please confirm your password. ")
-    if confirm == password:
-        makeaccount = ["{}:{}".format(username, password)]
-        sheet.insert_row(makeaccount, 2)
-        print("Successfully registered.")
-        col = sheet.col_values(1)
-        col.pop(0)
-        login()
-    elif confirm != password:
-        print("Your passwords do not match up! Please try again.")
-        register()
+    regUser = usernameEntry.get()
+    regPass = passwordEntry.get()
+    makeaccount = ["{}:{}".format(regUser, regPass)]
+    sheet.insert_row(makeaccount, 2)
+    hellore = "Incorrect username or password.".format(usernameEntry.get(), passwordEntry.get())
+    re = Label(frame, text="Successfully Registered!", bg="#e6e6e6", fg="green")
+    re.grid(row=4, column=1, columnspan=2)
+    col = sheet.col_values(1)
+    col.pop(0)
+    print("Successfully registered.")
+    goback()
+    print(makeaccount)
 
-already = input("Hello and Welcome to Proto! Do you have a an account with us? (y/n): ")
-if already.lower().strip() == "n":
-    register()
-if already.lower().strip() == "y":
-    login()
+
+def sign_in():
+    NoAccount.destroy()
+    myButton.destroy()
+    NewButt = Button(frame, text="Register", command=register, width=12, height=2, font=("Ariel", 10),
+                     bg="#e6e6e6")
+    NewButt.grid(row=3, column=2)
+    MakeAccount = Button(frame, text="  Have an account?   \n Log in now.", font=("Ariel", 11), bg="#e6e6e6",
+                         command=goback)  # command is what it does (Refer to function)
+    MakeAccount.grid(row=3, column=1, )
+    print("ypo")
+
+
+NoAccount = Button(frame, text="Don't have an account? \n Sign up now for free.", font=("Ariel", 10), bg="#e6e6e6",
+                   command=sign_in)  # command is what it does (Refer to function)
+NoAccount.grid(row=3, column=1, )
+myButton = Button(frame, text="Log In", command=myClick, width=12, height=2, font=("Ariel", 10),
+                  bg="#e6e6e6")  # command is what it does (Refer to function)
+myButton.grid(row=3, column=2)  # There needs no inside () for functions using commands ^
+
+# state=DISABLED (right after "text="Touch me!",) will turn the button into a locked buttom
+
+
+root.mainloop()
